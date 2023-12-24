f3ed3d3fff3cd62909b3624189cee55a8ba1b491
frank <a123456wyz@163.com>
2023-12-24 18:44:30 +0400

    -----

--- src/06-react-redux/views/NotFound.js
+++ src/06-react-redux/views/NotFound.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useEffect } from 'react'
 
-export default function NotFound() {
+function NotFound() {
+  useEffect(() => { }, [])
   return (
     <div>
       404 not found
     </div>
   )
 }
+
+function frankconnect(cb,obj) {
+  var value = cb()
+  return (MyComponent) => {
+    // 路由属性
+    return (props) => {
+      console.log('value: ', value);
+      console.log('props: ', props);
+      return <div style={{color:'red'}}> 
+        <MyComponent {...value} {...props}  {...obj} />
+      </div>
+    }
+  }
+}
+export default frankconnect(() => {
+  return {
+    a: 1,
+    b: 1
+  }
+},{ aa(){}, bb(){} })(NotFound)
