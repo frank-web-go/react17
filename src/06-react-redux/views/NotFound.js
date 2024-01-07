31a3840448937502a455b0c12e8f6fcdc348ae50
frank <a123456wyz@163.com>
2024-01-07 17:24:59 +0400

    ---

--- src/06-react-redux/views/NotFound.js
+++ src/06-react-redux/views/NotFound.js
@@ -1,8 +1,11 @@
+// HOC   high order component
 import React from 'react'
 import { useEffect } from 'react'
 
-function NotFound() {
-  useEffect(() => { }, [])
+function NotFound(props) {
+  useEffect(() => { 
+    console.log('props: ', props);
+  }, [props])
   return (
     <div>
       404 not found
@@ -13,10 +16,10 @@ function NotFound() {
 function frankconnect(cb,obj) {
   var value = cb()
   return (MyComponent) => {
-    // 路由属性
+    // 路由属性  return 一个函数式组件(一个整体)
     return (props) => {
-      console.log('value: ', value);
-      console.log('props: ', props);
+      // console.log('value: ', value);
+      // console.log('props: ', props);
       return <div style={{color:'red'}}> 
         <MyComponent {...value} {...props}  {...obj} />
       </div>
